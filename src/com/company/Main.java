176fcc3f207003748ea98e87f6b3c4148d073719
1 <a1@MacBook-Air-Mac.local>
2017-09-26 23:02:21 +0200

    Add parameters n_back

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,68 +1,76 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 class N_Back {
-    static String instruction =
-            "This is the learning session and our will see sequence of  10 random letters. After FIRST 3 letters, your task will be click 'Y' " +
-            "if current letter is the same than the second letter before.\n" +
-            "In other case you have to click 'N'. On the end You will see your score.\n" +
-            "Do not care of the result of this session. The same task with different letter will be repeated.\n" +
-            "\n" +
-            "When your will be ready, click ENTER.\n" +
-            "\n" +
-            "GOOD LUCK :)\"";
-    private final String[] listOfLetters = {"d","b","d","w","k"};
+    //private int parameter;
+    private int size = 5;
     private final String answerPositive = "Y";
     private final String answerNegative = "N";
+    private final String quit = "Q";
     private final String optionsToChoose = "'Y' or 'N'?";
-    private final String whiteSpace = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    private static final int delay = 1000;
+    private final String whiteSpace= String.join("", Collections.nCopies(40, "\n"));;
+    private static final int delay = 2000;
     private int numberOfCorrectAnswers = 0;
-    private int size = 20;
     private double probability = 0.3;
+    private int result = size - 2;
+    private String instruction =
+            whiteSpace+"This is the learning session of the 'n-back' test, where you will see sequence of "+size+" random letters." + " " +
+                    "After FIRST 3 letters, your task will be to press 'Y' " +
+                    "if current letter is the same as the second letter before.\n" +
+                    "In other case you have to press 'N'. In the end You will see your score.\n" +
+                    "Don't mind the result of this session. The same task with different letter will be repeated.\n" +
+                    "\n" +
+                    "When your will be ready, click ENTER.\n" +
+                    "\n" +
+                    "GOOD LUCK :)\"";
 
     public void showInstruction(){
         System.out.println(instruction);
+        Scanner startTest = new Scanner(System.in);
+        startTest.nextLine();
+        System.out.println(whiteSpace);
     }
 
-    public void showListWithRandomLetter() throws InterruptedException {
+    public void showListWithRandomLetter(int parameter) throws InterruptedException {
         String participantAnswer;
         Scanner scanner = new Scanner(System.in);
+        List<String> listOfLetters =  prepareListOfRandomLetters(size, probability, parameter);
 
-        for(int numberOfLetter = 0; numberOfLetter < listOfLetters.length - 1; numberOfLetter++){
-            System.out.println(listOfLetters[numberOfLetter]);
+        for(int numberOfLetter = 0; numberOfLetter < listOfLetters.size() - 1; numberOfLetter++){
+            System.out.println("                 " + listOfLetters.get(numberOfLetter));
             Thread.sleep(delay);
             System.out.println(whiteSpace);
-            if(numberOfLetter < 2){
+            if(numberOfLetter < parameter){
                 continue;
             }
             System.out.println(optionsToChoose);
             participantAnswer = scanner.nextLine();
-            Boolean lettersEqual = listOfLetters[numberOfLetter].equals(listOfLetters[numberOfLetter - 2]);
+            Boolean lettersEqual = listOfLetters.get(numberOfLetter).equals(listOfLetters.get(numberOfLetter - parameter));
             String expectedAnswer = lettersEqual ? answerPositive : answerNegative;
             if(expectedAnswer.equalsIgnoreCase(participantAnswer)){
                 numberOfCorrectAnswers++;
+            }
+            else if (participantAnswer.equalsIgnoreCase(quit)){
+                break;
             }
             System.out.println(whiteSpace);
         }
 
     }
+
     public String getRandomLetter(){
         Random random = new Random();
         int letter = random.nextInt(26) + (byte)'a';
         return String.valueOf((char)letter);
     }
 
-    public List<String> prepareListOfRandomLetters(int size, double probability) {
+    public List<String> prepareListOfRandomLetters(int size, double probability, int parameter) {
         List<String> listOfRandomLetter = new ArrayList<>();
         for (int i = 0; i <= size; i++){
             double randomNumber = generateRandomNumber();
-            if(randomNumber <= probability && i >= 2){
-                listOfRandomLetter.add(listOfRandomLetter.get(i - 2));
+            if(randomNumber <= probability && i >= parameter){
+                listOfRandomLetter.add(listOfRandomLetter.get(i - parameter));
             }else{
                 listOfRandomLetter.add(getRandomLetter());
             }
@@ -75,56 +83,26 @@ class N_Back {
         return random.nextDouble();
     }
 
-
-    public void showTheResult(){
-        String theEnding = "Well done! You have " + numberOfCorrectAnswers + " correct answers from 5.";
+    public void showTheResult() throws InterruptedException {
+        String theEnding = "Well done! You have " + numberOfCorrectAnswers + " correct answers from "+result+".";
         System.out.println(theEnding);
+        Thread.sleep(delay);
     }
 }
 
 public class Main{
     public  static void main(String[] args) throws InterruptedException {
         N_Back session1 = new N_Back();
-//        session1.showInstruction();
-//        session1.showListWithRandomLetter();
-//        session1.showTheResult();
-        //session1.getRandomLetter();
-
+        session1.showInstruction();
+        session1.showListWithRandomLetter(2);
+        session1.showTheResult();
+        N_Back session2 = new N_Back();
+        session2.showInstruction();
+        session2.showListWithRandomLetter(3);
+        session2.showTheResult();
+        N_Back session3 = new N_Back();
+        session3.showInstruction();
+        session3.showListWithRandomLetter(4);
+        session3.showTheResult();
     }
 }
-
-
-//TODO: 1 level - 2n-back
-//TODO: Instruction for participants.
-//TODO: The task to do: Show the list of letters with 1s. breaks.
-/*
-1). instruction:
-TODO - "This is the learning session and our will see sequence of  10 random letters. Your task will be click 'Y' if current letter is the same than the second letter before.
-TODO    In other case you have to click 'N'. On the end You will see your score.
-TODO    Do not care of the result of this session. The same task with different letter will be repeated.
-
-TODO    When your will be ready, click ENTER.
-
-TODO    GOOD LUCK :)"
-
-2). prepare list with letters:
-    TODO - 10 random letters
-
-3). show each of the letter:
-    TODO - find way to collect the correct and wrong reactions
-
-4). wait for reaction: click 'Y' or 'N'
-    TODO - wait 1second
-    - Type of reactions:
-        TODO - correct reaction: if 2 letters ago was the same letter what is now, participant should click Y
-        TODO - correct reaction: if participant clicks N when the letter is not the same than 2 letters ago
-        - wrong reaction 1: if participant won't click ENTER when 2 letters ago was the same letter what is now
-        - wrong reaction 2: if participant click ENTER when the letter is not the same than 2 letters ago
-
-5). sum correct reactions:
-    TODO - put correct reaction to the list (correct pairs of letter or only letter when was reaction correct)
-    TODO - put wrong reaction (the same thing to do what with correct reaction)
-
-6). show results: how many good and bad reactions:
-    TODO - "Your results: corrects(sth) / wrong(sth)."
- */
