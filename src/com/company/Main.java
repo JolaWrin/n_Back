d78bc8504d11c1a6b12f5cb5dd514684dd59d33b
1 <a1@MacBook-Air-Mac.local>
2017-09-27 21:00:30 +0200

    Refactoring: add paramiter 'n'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.*;
 
 class N_Back {
-    //private int parameter;
+    private int n;
     private int size = 5;
     private final String answerPositive = "Y";
     private final String answerNegative = "N";
@@ -15,48 +15,50 @@ class N_Back {
     private double probability = 0.3;
     private int result = size - 2;
     private String instruction =
-            whiteSpace+"This is the learning session of the 'n-back' test, where you will see sequence of "+size+" random letters." + " " +
-                    "After FIRST 3 letters, your task will be to press 'Y' " +
-                    "if current letter is the same as the second letter before.\n" +
+            whiteSpace+"This is the learning session of the 'n-back' test, where you will see sequence of %s random letters." + " " +
+                    "After FIRST  %s letters, your task will be to press 'Y' " +
+                    "if current letter is the same as a letter %s questions before.\n" +
                     "In other case you have to press 'N'. In the end You will see your score.\n" +
                     "Don't mind the result of this session. The same task with different letter will be repeated.\n" +
                     "\n" +
                     "When your will be ready, click ENTER.\n" +
                     "\n" +
                     "GOOD LUCK :)\"";
+    public N_Back(int n){
+        this.n = n;
+
+    }
 
     public void showInstruction(){
-        System.out.println(instruction);
+        System.out.println(String.format(instruction, size, n + 1, n));
         Scanner startTest = new Scanner(System.in);
         startTest.nextLine();
         System.out.println(whiteSpace);
     }
 
-    public void showListWithRandomLetter(int parameter) throws InterruptedException {
+    public void showListWithRandomLetter() throws InterruptedException {
         String participantAnswer;
         Scanner scanner = new Scanner(System.in);
-        List<String> listOfLetters =  prepareListOfRandomLetters(size, probability, parameter);
+        List<String> listOfLetters =  prepareListOfRandomLetters(size, probability);
 
         for(int numberOfLetter = 0; numberOfLetter < listOfLetters.size() - 1; numberOfLetter++){
             System.out.println("                 " + listOfLetters.get(numberOfLetter));
             Thread.sleep(delay);
             System.out.println(whiteSpace);
-            if(numberOfLetter < parameter){
+            if(numberOfLetter < n){
                 continue;
             }
             System.out.println(optionsToChoose);
             participantAnswer = scanner.nextLine();
-            Boolean lettersEqual = listOfLetters.get(numberOfLetter).equals(listOfLetters.get(numberOfLetter - parameter));
+            Boolean lettersEqual = listOfLetters.get(numberOfLetter).equals(listOfLetters.get(numberOfLetter - n));
             String expectedAnswer = lettersEqual ? answerPositive : answerNegative;
             if(expectedAnswer.equalsIgnoreCase(participantAnswer)){
                 numberOfCorrectAnswers++;
-            }
-            else if (participantAnswer.equalsIgnoreCase(quit)){
+            } else if (participantAnswer.equalsIgnoreCase(quit)){
                 break;
             }
             System.out.println(whiteSpace);
         }
-
     }
 
     public String getRandomLetter(){
@@ -65,12 +67,12 @@ class N_Back {
         return String.valueOf((char)letter);
     }
 
-    public List<String> prepareListOfRandomLetters(int size, double probability, int parameter) {
+    public List<String> prepareListOfRandomLetters(int size, double probability) {
         List<String> listOfRandomLetter = new ArrayList<>();
         for (int i = 0; i <= size; i++){
             double randomNumber = generateRandomNumber();
-            if(randomNumber <= probability && i >= parameter){
-                listOfRandomLetter.add(listOfRandomLetter.get(i - parameter));
+            if(randomNumber <= probability && i >= n){
+                listOfRandomLetter.add(listOfRandomLetter.get(i - n));
             }else{
                 listOfRandomLetter.add(getRandomLetter());
             }
@@ -92,17 +94,17 @@ class N_Back {
 
 public class Main{
     public  static void main(String[] args) throws InterruptedException {
-        N_Back session1 = new N_Back();
+        N_Back session1 = new N_Back(2);
         session1.showInstruction();
-        session1.showListWithRandomLetter(2);
+        session1.showListWithRandomLetter();
         session1.showTheResult();
-        N_Back session2 = new N_Back();
+        N_Back session2 = new N_Back(3);
         session2.showInstruction();
-        session2.showListWithRandomLetter(3);
+        session2.showListWithRandomLetter();
         session2.showTheResult();
-        N_Back session3 = new N_Back();
+        N_Back session3 = new N_Back(4);
         session3.showInstruction();
-        session3.showListWithRandomLetter(4);
+        session3.showListWithRandomLetter();
         session3.showTheResult();
     }
 }
